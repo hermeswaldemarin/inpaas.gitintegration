1ed11967ca604956056611c4a20527657af5805e
Local Dev <hermes@touchpoints.com>
2018-04-02 17:36:02 -0300

    Comentário

---
+++ sources/inpaas.devstudio.dynaform.studio.js
@@ -0,0 +1,48 @@
+/**
+ * Auto-generated comment stub
+ *
+  * @inpaas.key inpaas.devstudio.dynaform.studio
+ * @inpaas.name StudioFormImpl
+ * @inpaas.version 0.2
+ * @inpaas.type patterntype.form
+ * @inpaas.engine Nashorn
+ * @inpaas.anonymous false
+ * @inpaas.final false
+*/
+
+(function(){
+    var TO = Java.type("br.com.inpaas.fw.model.TO");
+
+    var ValidatorException = Java.type("br.com.inpaas.entity.validator.ValidatorException");
+  	var FormBusinessDelegate = Java.type("br.com.inpaas.forms.businessdelegate.FormBusinessDelegate");
+  
+    var DynaFormBusinessDelegate = Java.extend(Java.type("br.com.inpaas.forms.businessdelegate.DynaFormBusinessDelegate")); 
+
+    var instance = new DynaFormBusinessDelegate(scriptContext, form) 
+    	afterSet: function(data){
+        	logging.info("StudioFormImpl::afterSet::data", data);	  
+        },
+    	get: function(id) {
+			
+			var app = require("inpaas.core.entity.dao").getDao("CORE_APPLICATION").filter("id_formulario").equalsTo(id).find("SELECT CORE_APPLICATION.DS_KEY, CORE_APPLICATION.DO_DEVELOPER FROM CORE_APPLICATION INNER JOIN CORE_MODULE ON (CORE_MODULE.ID_APPLICATION = CORE_APPLICATION.ID_APPLICATION) INNER JOIN FORMULARIO ON (FORMULARIO.ID_MODULE = CORE_MODULE.ID_MODULE) WHERE ID_FORMULARIO = #id_formulario# ").first();
+
+			if (app.do_developer != "Y") {
+				// return null;
+				
+				var PermissionException = Java.type("br.com.inpaas.businessdelegate.exception.PermissionException");
+				
+				throw new PermissionException("error.permission.design.denied");
+			}
+    		
+    		return _super.get(id);
+    	},
+        delete : function(id) {
+			var formBd = new FormBusinessDelegate(scriptContext);
+			formBd.delete(id);
+        }
+    };
+
+    var _super = Java.super(instance);
+
+    return instance;
+})();
