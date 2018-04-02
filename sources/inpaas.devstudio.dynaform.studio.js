8bb29c5093097bcd49d232191869ab250dc8e85c
Local Dev <hermes@touchpoints.com>
2018-04-02 17:46:34 -0300

    Comentário

--- sources/inpaas.devstudio.dynaform.studio.js
+++ sources/inpaas.devstudio.dynaform.studio.js
@@ -3,7 +3,7 @@
  *
   * @inpaas.key inpaas.devstudio.dynaform.studio
  * @inpaas.name StudioFormImpl
- * @inpaas.version 0.8
+ * @inpaas.version 0.9
  * @inpaas.type patterntype.form
  * @inpaas.engine Nashorn
  * @inpaas.anonymous false
@@ -38,7 +38,7 @@
 			formBd.delete(id);
         },
         afterSet: function(data){
-        	logging.info("StudioFormImpl::afterSet::data");  
+        	logging.error("StudioFormImpl::afterSet::data");  
           	return data;
         }
     };
